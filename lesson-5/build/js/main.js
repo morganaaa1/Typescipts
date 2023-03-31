6ffd0a1e37d09468589c07025cdd606278e719e5
ali <alinabilahramadhan@gmail.com>
2023-03-31 10:46:40 +0700

    double casting and the unknown type

---
+++ lesson-5/build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// convert to more less specific types
+let a = "Hello";
+let b = a; // less specific type
+let c = a; // more specific type
+let d = 'World';
+let e = 'World';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(1, 2, 'concat');
+// be careful! TS sees no problem here - but a string is returned
+let nextVal = addOrConcat(1, 2, 'concat');
+10;
+10;
---
+++ lesson-5/src/main.ts
@@ -0,0 +1,25 @@
+type One = string
+type Two = string | number
+type Three = "Hello"
+
+// convert to more less specific types
+
+let a: One = "Hello"
+let b = a as Two // less specific type
+let c = a as Three // more specific type
+
+let d = <One>'World'
+let e = <string | number>'World'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+    if (c === 'add') return a + b
+        return '' + a + b
+}
+
+let myVal: string = addOrConcat(1, 2, 'concat') as string
+
+// be careful! TS sees no problem here - but a string is returned
+let nextVal: number = addOrConcat(1, 2, 'concat') as number
+
+10 as string
+(10 as unknown) as string
