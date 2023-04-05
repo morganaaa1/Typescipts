f9cc90f822f4cd3ec64bae6a3b56009f7727c07c
ali <alinabilahramadhan@gmail.com>
2023-04-05 13:15:27 +0700

    index signature syntax and readonly modifier

---
+++ lesson-7/src/main.ts
@@ -0,0 +1,36 @@
+// Index Signature
+
+
+interface TransactionObj {
+    readonly [index: string]: number;
+    
+}
+// interface TransactionObj {
+//     Pizza: number;
+//     Books: number;
+//     Job: number;
+// }
+
+const todaysTransactions: TransactionObj = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+}
+
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+
+let prop: string = 'Pizza';
+console.log(todaysTransactions[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+    let total = 0;
+    for (let prop in transactions) {
+        total += transactions[prop];
+    }
+    return total;
+}
+
+console.log(todaysNet(todaysTransactions));
+
+// todaysTransactions.Pizza = 40;
