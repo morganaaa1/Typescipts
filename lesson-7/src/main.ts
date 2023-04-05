37857a20ae6533e57c4e558c5bd47146931ccf6e
ali <alinabilahramadhan@gmail.com>
2023-04-05 13:47:10 +0700

    optional properties

--- lesson-7/src/main.ts
+++ lesson-7/src/main.ts
@@ -1,20 +1,22 @@
 // Index Signature
 
 
+// interface TransactionObj {
+//     readonly [index: string]: number;
+    
+// }
 interface TransactionObj {
     readonly [index: string]: number;
-    
+    Pizza: number;
+    Books: number;
+    Job: number;
 }
-// interface TransactionObj {
-//     Pizza: number;
-//     Books: number;
-//     Job: number;
-// }
 
 const todaysTransactions: TransactionObj = {
     Pizza: -10,
     Books: -5,
-    Job: 50
+    Job: 50,
+    Ali: 42
 }
 
 console.log(todaysTransactions.Pizza);
@@ -36,3 +38,19 @@ console.log(todaysNet(todaysTransactions));
 // todaysTransactions.Pizza = 40;
 
 console.log(todaysTransactions['Ali']);
+
+
+interface Student {
+    [key: string]: string | number | number[] | undefined;
+    name: string;
+    GPA: number;
+    classes?: number[];
+}
+
+const student: Student = {
+    name: 'Ali',
+    GPA: 3.5,
+    classes: [100, 200]
+}
+
+// console.log(student.test);
