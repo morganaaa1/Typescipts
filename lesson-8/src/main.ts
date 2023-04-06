002e7ae6b8bd0d22210391a1fa91f2998ce784b0
ali <alinabilahramadhan@gmail.com>
2023-04-06 13:29:28 +0700

    isTrue with keyof assertion

--- lesson-8/src/main.ts
+++ lesson-8/src/main.ts
@@ -9,3 +9,28 @@ console.log(isObj('john'))
 console.log(isObj([1, 2, 3]))
 console.log(isObj({ name: 'john'}))
 console.log(isObj(null))
+
+const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false }
+    }
+    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return { arg, is: false }
+    }
+    return { arg, is: !!arg }
+}
+
+console.log(isTrue(false))
+console.log(isTrue(0))
+console.log(isTrue(true))
+console.log(isTrue(1))
+console.log(isTrue("Ali"))
+console.log(isTrue(''))
+console.log(isTrue(null))
+console.log(isTrue(undefined))
+console.log(isTrue({}))
+console.log(isTrue({ name: 'john'}))
+console.log(isTrue([]))
+console.log(isTrue([1, 2, 3]))
+console.log(isTrue(NaN))
+console.log(isTrue(-0))
