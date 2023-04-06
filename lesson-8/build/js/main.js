136946485fd2747e97f51ad7cdb7cbbf802b6edb
ali <alinabilahramadhan@gmail.com>
2023-04-06 13:21:57 +0700

    isObject example

--- lesson-8/build/js/main.js
+++ lesson-8/build/js/main.js
@@ -1,2 +1,10 @@
 "use strict";
 const echo = (arg) => arg;
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj(true));
+console.log(isObj('john'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'john' }));
+console.log(isObj(null));
