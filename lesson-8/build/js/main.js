04b25292c05951c1fca58b722ba7a882f1350ae7
ali <alinabilahramadhan@gmail.com>
2023-04-06 13:44:57 +0700

    class with generics

--- lesson-8/build/js/main.js
+++ lesson-8/build/js/main.js
@@ -98,3 +98,17 @@ const usersArray = [
 ];
 console.log(getUsersProperty(usersArray, "email"));
 console.log(getUsersProperty(usersArray, "username"));
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const store = new StateObject("Ali");
+console.log(store.state);
+store.state = "John";
--- lesson-8/src/main.ts
+++ lesson-8/src/main.ts
@@ -118,3 +118,23 @@ const usersArray = [
 
 console.log(getUsersProperty(usersArray, "email"))
 console.log(getUsersProperty(usersArray, "username"))
+
+class StateObject<T> {
+    private data: T;
+
+    constructor(value: T) {
+        this.data = value;
+    }
+
+    get state(): T {
+        return this.data;
+    }
+
+    set state(value: T) {
+        this.data = value;
+    }
+}
+
+const store = new StateObject("Ali")
+console.log(store.state)
+store.state = "John"
