0123d39a4153e3357550fa556dd9c4f6a6618475
ali <alinabilahramadhan@gmail.com>
2023-04-06 15:21:35 +0700

    required and readonly

--- lesson-9/build/js/main.js
+++ lesson-9/build/js/main.js
@@ -10,3 +10,10 @@ const assign1 = {
 };
 console.log(updateAssignment(assign1, { grade: 90 }));
 const assignGraded = updateAssignment(assign1, { grade: 90 });
+// Required and Readonly
+const recordAssignment = (assign) => {
+    // Send to database, etc.
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: true }));
--- lesson-9/src/main.ts
+++ lesson-9/src/main.ts
@@ -21,3 +21,14 @@ const assign1: Assignment = {
 
 console.log(updateAssignment(assign1, { grade: 90 }))
 const assignGraded: Assignment = updateAssignment(assign1, { grade: 90 })
+
+// Required and Readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+    // Send to database, etc.
+    return assign
+}
+
+const assignVerified: Readonly<Assignment> = { ...assignGraded, verified: true }
+
+recordAssignment({ ...assignGraded, verified: true })
