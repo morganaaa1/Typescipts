cabf18cbc9d4bd4f7e50b13bdc0d28ecc708d972
ali <alinabilahramadhan@gmail.com>
2023-04-10 13:05:33 +0700

    generic list example

--- lesson-11/src/App.tsx
+++ lesson-11/src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
 import Counter from "./components/Counter"
+import List from "./components/List"
 
 import { useState } from "react"
 
@@ -15,7 +16,8 @@ function App() {
         This is my section
       </Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
-    </>  
+      <List items={["Coffee", "Seblak", "Code"]} render={(item: string) => <span className="gold bold">{item}</span>} />
+      </>  
   )
 
 }
