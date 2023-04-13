442edd06155aa2d712570eec1848e75c071140d6
ali <alinabilahramadhan@gmail.com>
2023-04-13 10:42:55 +0700

    add useEffect

--- lesson-12/src/App.tsx
+++ lesson-12/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface User {
   id: number;
@@ -9,6 +9,14 @@ function App() {
   
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users: ', users)
+    
+
+    return () => console.log('unmounting')
+  }, [users])
 
   return (
     <>
